3ffdf091c1bd7795cf0cd1830fcebd1864303984
Maksym Hlanskov <makfiz@gmail.com>
2022-05-25 23:25:46 +0300

    up

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,20 +1,29 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileMenu = document.querySelector('.mob-menu');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  menuBtnRef.addEventListener("click", () => {
-    
+  const toggleMenu = () => {
     const isMenuOpen =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-      menuBtnRef.classList.toggle("is-open");
-    
-    menuBtnRef.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenuRef.classList.toggle("is-open");
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
-  });
+  };
 
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
